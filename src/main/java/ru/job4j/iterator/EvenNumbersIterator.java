1b41ee2df9c37be885b7cca8bffe6684cf8ffb6d
upsadin <upsadin@gmail.com>
2023-03-07 21:41:46 +0300

    .1.2.Итератор четных чисел [#150]

---
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -0,0 +1,36 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EvenNumbersIterator implements Iterator<Integer> {
+
+    private int[] data;
+    private int index;
+
+    public EvenNumbersIterator(int[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (index < data.length) {
+             while (data[index] % 2 != 0) {
+                if (index + 1 == data.length) {
+                 return false;
+                }
+            index++;
+            }
+        }
+        return index < data.length;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException("There is no matching element");
+        }
+        return data[index++];
+    }
+
+}
