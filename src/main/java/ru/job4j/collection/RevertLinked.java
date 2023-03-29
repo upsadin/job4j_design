0b9b6b5720d45bf8a54794e7ea8064b39149f4b9
upsadin <upsadin@gmail.com>
2023-03-29 22:35:42 +0300

    6. Перевернуть связанный список [#161]

---
+++ src/main/java/ru/job4j/collection/RevertLinked.java
@@ -0,0 +1,68 @@
+package ru.job4j.collection;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class RevertLinked<T> implements Iterable {
+    Node<T> head;
+
+    public void add(T value) {
+        Node<T> newNode = new Node(value, null);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node<T> tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = newNode;
+    }
+
+    public boolean revert() {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        Node<T> prev = head.next;
+        Node<T> next = null;
+        while (prev != null) {
+            head.next = next;
+            next = head;
+            head = prev;
+            prev = head.next;
+        }
+        head.next = next;
+        return true;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new Iterator() {
+            Node<T> node = head;
+            @Override
+            public boolean hasNext() {
+                return node != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T value = node.item;
+                node = node.next;
+                return value;
+            }
+        };
+    }
+
+    private static class Node<T> {
+        T item;
+        Node<T> next;
+
+        public Node(T value, Node next) {
+            this.item = value;
+            this.next = next;
+        }
+    }
+}
