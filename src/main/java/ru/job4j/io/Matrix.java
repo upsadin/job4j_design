2209d9f1662ed31ddeed9ff18e3fe664d5dfd2c9
upsadin <upsadin@gmail.com>
2023-05-25 14:37:36 +0300

    2.0. FileOutputStream [#252488]

--- src/main/java/ru/job4j/io/Matrix.java
+++ src/main/java/ru/job4j/io/Matrix.java
@@ -11,7 +11,7 @@ public class Matrix {
             byte[][] table = new byte[size][size];
             for (int i = 0; i < size; i++) {
                 for (int j = 0; j < size; j++) {
-            out.write((byte)((i + 1) * (j + 1)));
+            out.write((byte) ((i + 1) * (j + 1)));
                 }
                 out.write(System.lineSeparator().getBytes());
             }
