271c84ef58394e3f7342b89ae30187f594567c59
upsadin <upsadin@gmail.com>
2023-05-25 14:36:21 +0300

    2.0. FileOutputStream [#252488]

---
+++ src/main/java/ru/job4j/io/Matrix.java
@@ -0,0 +1,22 @@
+package ru.job4j.io;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Matrix {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("data/matrix.txt")) {
+            int size = 9;
+            byte[][] table = new byte[size][size];
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+            out.write((byte)((i + 1) * (j + 1)));
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
