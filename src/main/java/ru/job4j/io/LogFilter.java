b96d3891bdcec877f8e1709d5898bac4974b5ddf
upsadin <upsadin@gmail.com>
2023-05-29 22:59:00 +0300

    5.1. BufferedReader [#252489]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -22,7 +22,7 @@ public class LogFilter {
         return rsl;
     }
 
- /*   public static void save(List<String> log, String file) {
+    public static void save(List<String> log, String file) {
         try (BufferedWriter out = new BufferedWriter(new FileWriter(file))) {
             for (String string : log) {
                 out.write(string + System.lineSeparator());
@@ -30,7 +30,7 @@ public class LogFilter {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }*/
+    }
 
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
@@ -38,6 +38,6 @@ public class LogFilter {
         for (String string : log) {
             System.out.println(string);
         }
-    /*    save(log, "data/404.txt");*/
+        save(log, "data/404.txt");
     }
 }
