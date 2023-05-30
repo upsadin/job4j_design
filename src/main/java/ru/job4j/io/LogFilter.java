bd9f0d222c4c382efb2dcb94284bce149aa76c8c
upsadin <upsadin@gmail.com>
2023-05-30 23:38:54 +0300

    5.2. BufferedOutputStream [#252490  [#252490 #419545]]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -22,20 +22,21 @@ public class LogFilter {
         return rsl;
     }
 
-/*    public static void save(List<String> log, String file) {
-        try (BufferedWriter out = new BufferedWriter(new FileWriter(file))) {
-            for (String string : log) {
-                out.write(string + System.lineSeparator());
-            }
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedWriter(
+                        new FileWriter(file)
+                ))) {
+            log.forEach(out::println);
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }*/
+    }
 
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
         List<String> log = logFilter.filter("data/log.txt");
         log.forEach(System.out::println);
-/*        save(log, "data/404.txt");*/
+        save(log, "data/404.txt");
     }
 }
