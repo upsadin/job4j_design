e2e5b0555c9a752ec672d6353ffd8f1a5ecc0303
upsadin <upsadin@gmail.com>
2023-06-25 22:06:09 +0300

    14. Архивировать проект [#861 # [#861]]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -34,6 +35,20 @@ public class Zip {
         }
     }
 
+    public static void validate(ArgsName arguments) {
+        Path path = Path.of(arguments.get("d"));
+        if (!Files.exists(path)) {
+            throw new IllegalArgumentException(String.format("%s doesn't exist", path.toAbsolutePath()));
+        }
+        if (!Files.isDirectory(path)) {
+            throw new IllegalArgumentException(String.format("%s is not a directory", path.toAbsolutePath()));
+        }
+        String exclude = arguments.get("e");
+        if (exclude.length() < 2 || !exclude.startsWith(".") || !Character.isLetter(exclude.charAt(1))) {
+            throw new IllegalArgumentException(String.format("File extension \'%s\' is incorrect", exclude));
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         Zip zip = new Zip();
 /*        zip.packSingleFile(
@@ -41,8 +56,9 @@ public class Zip {
                 new File("./pom.zip")
         );*/
         ArgsName arguments = ArgsName.of(args);
+        validate(arguments);
         Path root = Paths.get(arguments.get("d"));
-        Predicate<Path> condition = p -> !p.getFileName().endsWith(arguments.get("e"));
+        Predicate<Path> condition = p -> !p.getFileName().toString().endsWith(arguments.get("e"));
         List<Path> sources = Search.search(root, condition);
         zip.packFiles(sources, new File(arguments.get("o")));
     }
