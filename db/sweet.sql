25a7b1ae3e24cf2c325416fae1fa39cb1b8690d9
upsadin <upsadin@gmail.com>
2023-08-17 19:52:49 +0300

    0. Что такое база данных. [#6860]

--- db/sweet.sql
+++ db/sweet.sql
@@ -1,1 +1,9 @@
-select * from sweet;
+create table sweet(
+	id serial primary key,
+	name varchar(255),
+	cal int,
+	price float
+);
+insert into sweet (name, cal, price) values ('donat', 200, 55.5);
+update sweet set price = 70.35;
+delete from sweet;
