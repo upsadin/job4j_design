d93178ddd9268e18d5ae235f8c619cd5b3744e3d
upsadin <upsadin@gmail.com>
2023-08-23 20:36:06 +0300

    1. Хранилище машин [#1733]

--- db/cars.sql
+++ db/cars.sql
@@ -27,9 +27,14 @@ insert into car_engines(name) values('карбюратор'), ('инжектор
 insert into car_transmissions(name) values('механическая'), ('автомат'), ('вариатор');
 insert into cars(name, body_id, engine_id, transmission_id) values
     ('Модель 1', 1, 1, 1), ('Модель 2', 2, 2, 2), ('Модель 3', 3, 3, 1), ('Модель 4', 2, 4, 2), ('Модель 5', 1, 1, 2);
-SELECT c.id, c.name, cb.name, ce.name, ct.name FROM cars c full join car_bodies cb on c.body_id = cb.id
+create view vivod_vseh_dannih
+as SELECT c.id, c.name as "Машины", cb.name as "Кузов" , ce.name as "Двигатель", ct.name as "Коробка"
+FROM cars c full join car_bodies cb on c.body_id = cb.id
     full join car_engines ce on c.engine_id = ce.id
     full join car_transmissions ct on c.transmission_id = ct.id;
+
+select * from vivod_vseh_dannih;
+
 SELECT cb.name as "Кузов не используется" FROM car_bodies cb left join cars c on c.body_id = cb.id where c.name is null;
 SELECT ce.name as "Двигатель не используется" FROM car_engines ce left join cars c on c.engine_id = ce.id where c.name is null;
 SELECT ct.name as "Коробка не используется" FROM car_transmissions ct left join cars c on c.transmission_id = ct.id where c.name is null;
