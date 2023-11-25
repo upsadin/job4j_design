d06a3a7e45a219ceb4c16d6db24825ad56e621d6
upsadin <upsadin@gmail.com>
2023-11-25 18:02:31 +0300

    4. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/SimpleGeneratorTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.template;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Disabled
+class SimpleGeneratorTest {
+    @Test
+    public void whenProduceTemplateWithValidKeys() {
+        SimpleGenerator generator = new SimpleGenerator();
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Pavel");
+        map.put("subject", "you");
+        String rsl = generator.produce("I am a ${name}, Who are ${subject}?", map);
+        assertThat(rsl).isEqualTo("I am Pavel, Who are you?");
+    }
+
+    @Test
+    public void whenProduceTemplateWithInvalidKeys() {
+        SimpleGenerator generator = new SimpleGenerator();
+        Map<String, String> map = new HashMap<>();
+        map.put("surnname", "Pavel");
+        map.put("subject", "you");
+        String rsl = generator.produce("I am a ${name}, Who are ${subject}?", map);
+        assertThatThrownBy(() -> generator.produce("I am a ${name}, Who are ${subject}?", map))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("неверный ключ \"name\"");
+    }
+
+    @Test
+    public void whenProduceTemplateWithOddKey() {
+        SimpleGenerator generator = new SimpleGenerator();
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Pavel");
+        map.put("surnname", "Pavlov");
+        map.put("subject", "you");
+        String rsl = generator.produce("I am a ${name}, Who are ${subject}?", map);
+        assertThatThrownBy(() -> generator.produce("I am a ${name}, Who are ${subject}?", map))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Передан лишний ключ \"surnname\"");
+    }
+
+}
